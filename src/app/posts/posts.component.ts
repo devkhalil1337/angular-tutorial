ac9408240478fa3ba009ee1e89ba6a8d4429ce3e
Khalil Asif <k.asif@ssidecisions.com>
2021-08-26 20:19:05 +0500

    delete http call

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,9 +9,9 @@ import { Http } from '@angular/http';
 export class PostsComponent {
 
   posts;
-
+  url = "https://jsonplaceholder.typicode.com/posts/";
   constructor(private http:Http) { 
-    http.get("https://jsonplaceholder.typicode.com/posts").subscribe(response => {
+    http.get(this.url).subscribe(response => {
       this.posts = response.json();
     })
   }
@@ -20,7 +20,7 @@ export class PostsComponent {
   createPost(input: HTMLInputElement){
     let post = {title: input.value};
     input.value = '';
-    this.http.post("https://jsonplaceholder.typicode.com/posts",JSON.stringify(post)).subscribe(response => {
+    this.http.post(this.url,JSON.stringify(post)).subscribe(response => {
       post['id'] = response.json().id;
       this.posts.splice(0,0,post);
     });
@@ -29,8 +29,15 @@ export class PostsComponent {
   updatePost(topic: HTMLInputElement){
     console.log(topic.value)
     let post = {title:topic.value}
-    this.http.put("https://jsonplaceholder.typicode.com/posts/1",JSON.stringify(post)).subscribe(response => {
+    this.http.put(this.url + "/1",JSON.stringify(post)).subscribe(response => {
 
+    });
+  }
+
+  deletePost(post){
+    this.http.delete(this.url +"/"+ post.id).subscribe(response => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index,1);
     });
   }
   
